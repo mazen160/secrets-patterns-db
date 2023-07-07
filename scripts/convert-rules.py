eb43f5e39a504da7463693040b358c608b6fffd4
eL-Prova <wimeikelboom100@hotmail.com>
2023-07-07 14:32:36 +0200

    Update convert-rules.py
    Adding argument parser
    Make option to export to file otherwise print

--- scripts/convert-rules.py
+++ scripts/convert-rules.py
@@ -2,19 +2,20 @@
 import sys
 import json
 import yaml
+import argparse
 
 
-def print_trufflehog_output(y):
+def trufflehog_output(y):
     output = {}
     for i in y["patterns"]:
         if i["pattern"]["confidence"] != "high":
             continue
         output.update({i["pattern"]["name"]: i["pattern"]["regex"]})
+    
+    return json.dumps(output, indent=4, sort_keys=True)
+    
 
-    print(json.dumps(output, indent=4, sort_keys=True))
-
-
-def print_gitleaks_output(y):
+def gitleaks_output(y):
     s = 'title = "gitleaks config"'
 
     for i in y["patterns"]:
@@ -26,29 +27,36 @@ def print_gitleaks_output(y):
     regex = '''{i["pattern"]["regex"]}'''
     tags = ["secret"]
 """
-    print(s)
+    return s
 
 
-def main():
-
-    if len(sys.argv) < 3:
-        print(f"\nUsage:\n\t{sys.argv[0]} [regex-db.yml] [output-type]")
-        print("Supported output types: trufflehog, gitleaks")
-        exit(1)
-
-    f = open(sys.argv[1], "r")
+def main(arg):
+    f = open(arg.database_file, "r")
     y = yaml.safe_load(f.read())
     f.close()
-
-    OUTPUT_TYPE = sys.argv[2]
-    if OUTPUT_TYPE not in ("trufflehog", "gitleaks"):
-        print("Invalid output type")
-        exit(1)
-    if OUTPUT_TYPE == "trufflehog":
-        print_trufflehog_output(y)
-    elif OUTPUT_TYPE == "gitleaks":
-        print_gitleaks_output(y)
+    
+    output_string = ""
+    ext_string = ""
+    if arg.output_type == "trufflehog":
+        output_string = trufflehog_output(y)
+        ext_string = "json"
+    elif arg.output_type == "gitleaks":
+        output_string = gitleaks_output(y)
+        ext_string = "toml"
+    
+    if arg.export_filename is not None:              
+        f = open(f"{arg.export_filename}.{ext_string}", "w")
+        f.write(output_string)
+        f.close()
+    else:
+        print(output_string)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Convert yaml database file to rules for trufflehog or gitleaks')
+    parser.add_argument("--db", dest = "database_file", required = True, help = "The yaml database file")
+    parser.add_argument("--type", dest= "output_type", required = True, choices=['trufflehog', 'gitleaks'], help = "Supported output types: trufflehog, gitleaks")
+    parser.add_argument('--export', dest="export_filename", help = "Give filename, extension toml/json will be added")
+    args = parser.parse_args()
+    
+    main(args)
