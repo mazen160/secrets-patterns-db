3f9f67a8f2b6e140a50a226041e9593fc2f5637e
Mazin Ahmed <mazen160@users.noreply.github.com>
2023-10-17 20:52:25 +0400

    Dev (#16)
    * Dev
    
    * Added dataset of datasets/cabinjs_sensitive-fields.json
    
    * Added sensitive fields
    
    * Added sort-fields.py
    
    * PII update
    
    * rules updates
    
    * Rules update
    
    * Tested and cleaned rules for PII
    
    * removed rule
    
    * Create sample-test-1.txt

---
+++ scripts/sort-fields.py
@@ -0,0 +1,36 @@
+import yaml, sys
+
+with open(sys.argv[1], "r") as f:
+    data = f.read()
+    data = yaml.safe_load(data)
+
+new_data = []
+
+output = []
+for i in data["fields"]:
+    i = i.lower()
+    if " " in i:
+        output.append(i.replace(" ", "-"))
+        output.append(i.replace(" ", "_"))
+    if "-" in i:
+        output.append(i.replace(" ", " "))
+        output.append(i.replace(" ", "_"))
+    if "_" in i:
+        output.append(i.replace(" ", " "))
+        output.append(i.replace(" ", "-"))
+
+    if " " in i:
+        new_data.extend(i.split(" "))
+    output.append(i)
+
+output.extend(new_data)
+output = list(set(output))
+
+
+class MyDumper(yaml.Dumper):
+    def increase_indent(self, flow=False, indentless=False):
+        return super(MyDumper, self).increase_indent(flow, False)
+
+yaml.dump({"fields": output}, sys.stdout,
+          Dumper=MyDumper,
+          default_flow_style=False, sort_keys=True)
